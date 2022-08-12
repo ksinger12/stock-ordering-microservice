649aa44719e969a8b5a75f6d131eadad2f86c71c
kingsing <king.ks1919@gmail.com>
2022-08-12 14:22:45 +0000

    adding intialized spring project

---
+++ trader/src/test/java/com/trading/trader/TraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading.trader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
