106ccaf96b3dbde410b2684c4f3436082b50df9e
syeo-1 <seanyeo.work@gmail.com>
2022-08-12 18:41:43 +0000

    add more structure

---
+++ trader/src/main/java/com/trading/trader/service/HoldingServiceImpl.java
@@ -0,0 +1,35 @@
+package com.trading.trader.service;
+
+import com.trading.trader.entities.Holding;
+
+public class HoldingServiceImpl implements HoldingService {
+    @Override
+    public Iterable<Holding> getAllHoldings() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Holding> getAllTickerHoldings(String tickerSymbol) {
+        return null;
+    }
+
+    @Override
+    public Holding addNewHolding(Holding Holding) {
+        return null;
+    }
+
+    @Override
+    public void deleteHolding(int id) {
+
+    }
+
+    @Override
+    public void deleteHolding(Holding Holding) {
+
+    }
+
+    @Override
+    public Holding updateHolding(Holding Holding) {
+        return null;
+    }
+}
--- trader/src/main/java/com/trading/trader/service/TradeServiceImpl.java
+++ trader/src/main/java/com/trading/trader/service/TradeServiceImpl.java
@@ -1,4 +1,40 @@
 package com.trading.trader.service;
 
-public class TradeServiceImpl implements TradeService{
+import com.trading.trader.entities.Trade;
+
+public class TradeServiceImpl implements TradeService {
+    @Override
+    public Iterable<Trade> getAllTrades() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Trade> getAllTickerTrades(String tickerSymbol) {
+        return null;
+    }
+
+    @Override
+    public Iterable<Trade> getTradesInDateRange(String startTime, String endTime) {
+        return null;
+    }
+
+    @Override
+    public Trade addNewTrade(Trade trade) {
+        return null;
+    }
+
+    @Override
+    public void deleteTrade(int id) {
+
+    }
+
+    @Override
+    public void deleteTrade(Trade trade) {
+
+    }
+
+    @Override
+    public Trade updateTrade(Trade trade) {
+        return null;
+    }
 }
