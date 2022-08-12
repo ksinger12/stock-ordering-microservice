90f80e5448f661e006deef01c9a853d663575874
kingsing <king.ks1919@gmail.com>
2022-08-12 17:43:48 +0000

    implementing and testing external api

---
+++ trader/src/main/java/com/trading/trader/AlphaVantageConfig.java
@@ -0,0 +1,5 @@
+package com.trading.trader;
+
+public class AlphaVantageConfig {
+
+}
--- trader/src/test/java/com/trading/trader/TraderApplicationTests.java
+++ trader/src/test/java/com/trading/trader/TraderApplicationTests.java
@@ -1,5 +1,12 @@
 package com.trading.trader;
 
+import com.crazzyghost.alphavantage.AlphaVantage;
+import com.crazzyghost.alphavantage.Config;
+import com.crazzyghost.alphavantage.forex.Forex;
+import com.crazzyghost.alphavantage.parameters.Interval;
+import com.crazzyghost.alphavantage.parameters.OutputSize;
+import com.crazzyghost.alphavantage.timeseries.TimeSeries;
+import com.crazzyghost.alphavantage.timeseries.response.TimeSeriesResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +15,22 @@ class TraderApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Config cfg = Config.builder()
+				.key("RBJRR2YCUMFQRRKZ")
+				.timeOut(10)
+				.build();
+
+		AlphaVantage.api().init(cfg);
+
+		TimeSeriesResponse response = AlphaVantage.api()
+				.timeSeries()
+				.intraday()
+				.forSymbol("IBM")
+				.interval(Interval.FIVE_MIN)
+				.outputSize(OutputSize.FULL)
+				.fetchSync();
+		// AlphaVantage.api().fundamentalData().companyOverview().forSymbol("IBM").fetchSync(); // use for stock info
+		// AlphaVantage.api().sector().fetchSync() // use for sector info
 	}
 
 }
